f0a795b36ecd398b13d66b23e8bafc9f74533b0e
Gaurav Singh <gaurav@onpharma.org>
2026-01-12 02:21:22 +0530

    fix 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,14 +83,21 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
 
+    config.action_mailer.default_url_options = {
+      host: "citi-infoline-services.onrender.com",
+      protocol: "https"
+    }
+
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
+      domain: "onrender.com",
       user_name: "gaurav165ongraph@gmail.com",
-      password: "cokz chmy wwbs rvhp",
+      password: "cokzchmywwbsrvhp",
       authentication: "plain",
       enable_starttls_auto: true
     }
+
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
