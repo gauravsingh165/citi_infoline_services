4df7caafb95c2eda2b8cdb3eec214f2f2a4548fc
Gaurav Singh <gaurav@onpharma.org>
2026-01-12 14:30:45 +0530

    fix 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,24 +79,26 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
+ config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 
-    config.action_mailer.default_url_options = {
-      host: "citi-infoline-services.onrender.com",
-      protocol: "https"
-    }
+  config.action_mailer.default_url_options = {
+    host: "citi-infoline-services.onrender.com",
+    protocol: "https"
+  }
 
-    config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 465,
-      domain: "onrender.com",
-      user_name: "gaurav165ongraph@gmail.com",
-      password: "cokzchmywwbsrvhp",
-      authentication: "plain",
-      ssl: true
-    }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "onrender.com",
+    user_name: "gaurav165ongraph@gmail.com",
+    password: "cokzchmywwbsrvhp",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    open_timeout: 10,
+    read_timeout: 10
+  }
 
 
   # Don't log any deprecations.
